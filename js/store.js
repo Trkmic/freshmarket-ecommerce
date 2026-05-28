f8c23506ff4aa02866613b9f2f8af0f7e4eabe4d
Ignacio Trkmic <trkmicignacio@gmail.com>
2026-05-28 12:38:09 -0300

    chore: add missing /api suffix to production URL

--- js/store.js
+++ js/store.js
@@ -188,7 +188,7 @@ const VALID_COUPONS = {
 
 export const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:5000/api'
-    : 'https://freshmarket-ecommerce.onrender.com';
+    : 'https://freshmarket-ecommerce.onrender.com/api';
 
 class Store {
     constructor() {
